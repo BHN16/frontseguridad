27d8dc656a7fc5b7ce6d109199b9c2602410a34b
Viktor7421 <victor.ascuna@utec.edu.pe>
2022-05-27 17:17:54 -0500

    PlayWithReact

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './Assets/Img/Melgar.png';
 import './App.css';
+import Test from './Test.js';
 
 function App() {
+  let isOpen = false;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Melgar el más grande.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test isOpen = {isOpen}/>
       </header>
     </div>
   );
