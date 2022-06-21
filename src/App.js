03aba29fdfc1265e50ab881e326fbf3b8f0912e2
Viktor7421 <victor.ascuna@utec.edu.pe>
2022-06-20 19:00:22 -0500

    feat: Passwords cards padding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import { Routes, Route } from 'react-router-dom';
 
+function AES_Encrypt(key, text) {
+  var aesjs = require('aes-js');
+  var textBytes = aesjs.utils.utf8.toBytes(text);
+
+  var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+  var encryptedBytes = aesCtr.encrypt(textBytes);
+
+  var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+  return encryptedHex;
+}
+
+function AES_Decrypt(key, encryptedHex) {
+  var aesjs = require('aes-js');
+  var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+
+  var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+  var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+
+  var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+  return decryptedText;
+}
+
 function App() {
   return (
     <Routes>
